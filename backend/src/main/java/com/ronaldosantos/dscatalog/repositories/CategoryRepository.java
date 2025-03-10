f44f876fdb908a8fc926a721411c1b80ede28dfa
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-10 14:41:42 -0300

    H2 Console

---
+++ backend/src/main/java/com/ronaldosantos/dscatalog/repositories/CategoryRepository.java
@@ -0,0 +1,12 @@
+package com.ronaldosantos.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ronaldosantos.dscatalog.entities.Category;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
