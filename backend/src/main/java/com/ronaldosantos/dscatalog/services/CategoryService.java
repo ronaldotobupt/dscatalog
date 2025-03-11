acb3ea38dbb767230f5bf0b71f3e2791446456ca
Ronaldo Santos <ronaldo.tboupt@gmail.com>
2025-03-11 08:23:13 -0300

    Criando CategoryDTO

--- backend/src/main/java/com/ronaldosantos/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/ronaldosantos/dscatalog/services/CategoryService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.ronaldosantos.dscatalog.dto.CategoryDTO;
 import com.ronaldosantos.dscatalog.entities.Category;
 import com.ronaldosantos.dscatalog.repositories.CategoryRepository;
 
@@ -14,8 +16,10 @@ public class CategoryService {
 	@Autowired
 	CategoryRepository repository;
 	
-	public List<Category> findAll(){
-		return repository.findAll();
+	@Transactional(readOnly = true)
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).toList();
 	}
 
 }
